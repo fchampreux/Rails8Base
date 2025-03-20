880977e258f0b6adfdfa7843f4cccdc451e5b1d7
Frédéric CHAMPREUX <fchampreux@gmail.com>
2025-03-20 16:53:31 +0000

    Updated database creation scripts

--- .devcontainer/PG_Init_4_Rails_dev.sql
+++ .devcontainer/PG_Init_4_Rails_dev.sql
@@ -1,4 +1,2 @@
 -- Create schemas
 CREATE SCHEMA IF NOT EXISTS rails_app AUTHORIZATION rails_user;
--- Change database owner
-ALTER DATABASE rails_base_dev OWNER TO rails_user;
--- .devcontainer/db_init.sh
+++ .devcontainer/db_init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Automatically create users, databases and schemas for Rails 
 psql -h postgres -U postgres -w -f .devcontainer/PG_Init_4_Rails.sql
-psql -h postgres -U postgres -w -f .devcontainer/PG_Init_4_Rails_dev.sql
-psql -h postgres -U postgres -w -f .devcontainer/PG_Init_4_Rails_test.sql
-psql -h postgres -U postgres -w -f .devcontainer/PG_Init_4_Rails_prod.sql
+psql -h postgres -U postgres -d rails_base_dev  -f .devcontainer/PG_Init_4_Rails_dev.sql
+psql -h postgres -U postgres -d rails_base_test -f .devcontainer/PG_Init_4_Rails_test.sql
+psql -h postgres -U postgres -d rails_base_prod -f .devcontainer/PG_Init_4_Rails_prod.sql
 echo "Users, databases and schemas for Rails created!"
