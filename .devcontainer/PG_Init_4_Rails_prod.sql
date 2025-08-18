b7d51b3ebe55f8d51912e053c32a8a6aa086684a
Fred <frederic.champreux@opendataquality.com>
2025-08-18 13:06:52 +0200

    Base configuration updated to meet on premise use

--- .devcontainer/PG_Init_4_Rails_prod.sql
+++
@@ -1,2 +0,0 @@
--- Create schemas
-CREATE SCHEMA IF NOT EXISTS rails_app AUTHORIZATION rails_user;
--- .devcontainer/db_init.sh
+++ .devcontainer/db_init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# Automatically create users, databases and schemas for Rails 
-psql -h postgres -U postgres -w -f .devcontainer/PG_Init_4_Rails.sql
-psql -h postgres -U postgres -d rails_base_dev  -f .devcontainer/PG_Init_4_Rails_dev.sql
-psql -h postgres -U postgres -d rails_base_test -f .devcontainer/PG_Init_4_Rails_test.sql
-psql -h postgres -U postgres -d rails_base_prod -f .devcontainer/PG_Init_4_Rails_prod.sql
+# Automatically create users, databases and schemas for Rails Dev and Test environments
+# Sample: psql -h postgres -U postgres -w -f .devcontainer/PG_Init_4_Rails.sql
+psql  -U postgres -w -f PG_Init_4_Rails.sql
+psql  -U rails_user -d rails_base_dev  -f PG_Init_4_Rails_dev.sql
+psql  -U rails_user -d rails_base_test -f PG_Init_4_Rails_test.sql
 echo "Users, databases and schemas for Rails created!"
