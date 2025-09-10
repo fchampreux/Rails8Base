c4802654de66931accfa711573c1c293766880c6
Fred <frederic.champreux@opendataquality.com>
2025-09-10 22:26:43 +0200

    Removed container related configuration elements Refactored db_init script Restructured database.yml and credentials

--- db/init/db_init.sh
+++ db/init/db_init.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # Automatically create users, databases and schemas for Rails Dev and Test environments
 # Sample: psql -h postgres -U postgres -w -f .devcontainer/PG_Init_4_Rails.sql
-psql  -U postgres -w -f PG_Init_4_Rails.sql
-psql  -U rails_user -d rails_base_dev  -f PG_Init_4_Rails_dev.sql
-psql  -U rails_user -d rails_base_test -f PG_Init_4_Rails_test.sql
+export PGPASSFILE='./.pgpass'
+psql  -U postgres -w -f PG_Init_1_Rails.sql
+psql  -U rails_user -d rails_base_dev  -f PG_Init_2_Rails_dev.sql
+psql  -U rails_user -d rails_base_test -f PG_Init_3_Rails_test.sql
 echo "Users, databases and schemas for Rails created!"
