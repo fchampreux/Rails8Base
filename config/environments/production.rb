d4ddaeff8eb7d40a0358588790fad7b081382973
Frédéric CHAMPREUX <fchampreux@gmail.com>
2026-06-10 14:33:37 +0000

    Updated base configuration constants

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,8 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "opendataquality.com" }
+  # By default, uses the organisation's domain defined in config.rb
+  config.action_mailer.default_url_options = { host: $Organisation_domain }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   # config.action_mailer.smtp_settings = {
